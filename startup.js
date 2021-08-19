e7cbb4917cc3f2980d42ea58c2ccd976c9ba1db4
mark.exe <77983517+markverb1@users.noreply.github.com>
2021-08-19 14:58:53 +0300

    Create startup.js

---
+++ startup.js
@@ -0,0 +1,9 @@
+var myStorage = window.localStorage;
+var visitedBefore = localStorage.getItem('visitedBefore');
+console.log("WIP");
+function playStartup() {
+console.error("This is not a feature yet")
+}
+if (visitedBefore != true) {
+  playStartup()
+}
